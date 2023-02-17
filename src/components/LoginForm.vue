89c83b36a1222ec0f032b51b46480a6596f4411d
JJK <jjkm0222@gmail.com>
2023-02-17 21:25:32 +0900

    async,await 에러 핸들링, 이메일 validation

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,8 +1,14 @@
 <template>
   <form @submit.prevent="submitForm">
     <div>
+      <!-- 회원가입한 아이디, pw 입력 -->
       <label for="username">id:</label>
-      <input id="username" type="text" v-model="username" />
+      <input
+        id="username"
+        type="text"
+        placeholder="이메일 형식을 입력해주세요"
+        v-model="username"
+      />
     </div>
     <div>
       <label for="password">pw:</label>
@@ -29,15 +35,19 @@ export default {
       logMessage: '',
     };
   },
+  // computed 속성은 데이터 변화에 따라서, props가 됐건 data가 됐건 store가 됐건
+  // 자동으로 계산해준다
   computed: {
     isUsernameValid() {
+      // this.username이 한 글자 한글자 바뀔 때마다 validateEmail(this.username)이 실행된다
+      // true or false 반환
       return validateEmail(this.username);
     },
   },
   methods: {
     async submitForm() {
       try {
-        // 비즈니스 로직
+        // 비즈니스 로직, 필요한 로직
         const userData = {
           username: this.username,
           password: this.password,
@@ -48,6 +58,8 @@ export default {
         // this.initForm();
       } catch (error) {
         // 에러 핸들링할 코드
+        // Failed to load resource: the server responded with a status of 401 (Unauthorized)
+        // axios에 대한 post 요청에 대한 에러이기 때문에 에러 안에 response가 있다는 걸 알 수 있다
         console.log(error.response.data);
         this.logMessage = error.response.data;
         // this.initForm();
