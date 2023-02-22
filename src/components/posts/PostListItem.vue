be122cad76c5d4aa699c9633b0c298c6d512fff8
JJK <jjkm0222@gmail.com>
2023-02-22 21:50:08 +0900

    전역/지역 필터

--- src/components/posts/PostListItem.vue
+++ src/components/posts/PostListItem.vue
@@ -7,6 +7,7 @@
       {{ postItem.contents }}
     </div>
     <div class="post-time">
+      <!-- vue filter 기능. 전역필터 -->
       {{ postItem.createdAt | formatDate }}
       <i class="icon ion-md-create" @click="routeEditPage"></i>
       <i class="icon ion-md-trash" @click="deleteItem"></i>
@@ -24,6 +25,22 @@ export default {
       required: true,
     },
   },
+  // 따로 모듈화 해서 main.js에서 import해서 쓸거다
+  // main.js 가봐라
+  // 얘는 지역 필터
+  // filters: {
+  //   formatDate(value) {
+  //     const date = new Date(value);
+  //     const year = date.getFullYear();
+  //     let month = date.getMonth() + 1;
+  //     month = month > 9 ? month : `0${month}`;
+  //     const day = date.getDate();
+  //     let hours = date.getHours();
+  //     hours = hours > 9 ? hours : `0${hours}`;
+  //     const minutes = date.getMinutes();
+  //     return `${year}-${month}-${day} ${hours}:${minutes}`;
+  //   },
+  // },
   methods: {
     async deleteItem() {
       if (confirm('You want to delete it?')) {
--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from '@/routes/index';
 import store from '@/store/index';
+// filter 함수 가져오기
 import { formatDate } from '@/utils/filters';
 
+// 전역 filter로 설정
+// PostListItem.vue ㄱㄱ
 Vue.filter('formatDate', formatDate);
 Vue.config.productionTip = false;
 
